760b7a120fdd57c49c053a50cec686b6df6f2acd
SanjuJssmr <106820541+SanjuJssmr@users.noreply.github.com>
2022-11-19 07:51:02 +0530

    Update TeamMessage.jsx

--- src/components/TeamMessage.jsx
+++ src/components/TeamMessage.jsx
@@ -2,13 +2,11 @@ import React from "react";
 import { MessageSimple, useMessageContext } from "stream-chat-react";
 
 const TeamMessage = () => {
-  const { handleOpenThread, message } = useMessageContext();
 
   return (
-    <MessageSimple
-      message={{ ...message, user: {} }}
-      // handleOpenThread={}
-    />
+    <>
+    TeamMessage
+    </>
   );
 };
 
